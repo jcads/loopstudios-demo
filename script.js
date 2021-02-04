f3efb1a9b6446b3d396a927f57c1ce9a6e358ce3
rafael <dragonwood0@protonmail.com>
2021-02-04 15:14:25 +0800

    Navbar for mobile

---
+++ script.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector(".burger");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", navToggle);
+
+function navToggle(e) {
+  nav.classList.toggle("nav-active");
+}
